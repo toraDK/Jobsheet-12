e09ed5444e0c054fc7b459629d6796b7397f9fcf
tora180405 <toradigda@gmail.com>
2023-11-23 23:09:07 +0700

    initial commit

---
+++ TerimaKasih_27.java
@@ -0,0 +1,18 @@
+public class TerimaKasih_27 {
+
+    public static void UcapanTerimakasih(){
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan (ucapan);
+    }
+    
+}
