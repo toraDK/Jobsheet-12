5234f4e7029baa207d0889b602c9cf9e403e73b3
tora180405 <toradigda@gmail.com>
2023-11-23 23:08:48 +0700

    initial commit

---
+++ Programku.java
@@ -0,0 +1,30 @@
+public class Programku{
+
+    public static void TampilHinggaKei(int i) {
+
+        for (int j = 1; j <= i; j++) {
+            System.out.println(j);
+        }
+
+    }
+
+    public static int jumlah(int bil1, int bil2) {
+
+        return (bil1 + bil2);
+
+    }
+
+    public static void TampilJumlah(int bil1, int bil2) {
+
+        TampilHinggaKei(jumlah(bil1, bil2));
+
+    }
+
+    public static void main(String[] args) {
+
+        int temp = jumlah(1, 1);
+        TampilJumlah(temp, 5);
+
+    }
+
+}
